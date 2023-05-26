d6f5d8937689d24b5558d3298d600688930d57c3
Yassine Haouzane <haouzaneyassine@gmail.com>
2023-05-26 14:44:08 +0200

    feat(renderer): adding first version of sdl2 renderer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+mod renderer;
 mod vm;
+use renderer::handle_event;
+use renderer::initialize_sdl_renderer;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use std::env;
 use std::fs;
-use std::{thread, time};
+use std::thread;
+use std::time::Duration;
 use vm::*;
 
 fn read_rom(rom_path: &String) -> VM {
@@ -16,13 +22,19 @@ fn read_rom(rom_path: &String) -> VM {
     result
 }
 
-fn main() {
+fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     let mut virtual_machine = read_rom(&args[1]);
-    loop {
+    let (mut event_pump, mut canvas) = initialize_sdl_renderer().unwrap();
+    'running: loop {
         virtual_machine.decode_instruction();
-        let ten_millis = time::Duration::from_secs(1);
-
-        thread::sleep(ten_millis);
+        let event = handle_event(&mut event_pump, &mut canvas);
+        if event.is_some() {
+            break 'running;
+        }
+        canvas.clear();
+        canvas.present();
+        thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+    Ok(())
 }
